f69c588d07b550fc7068176285f9fa14259908a5
Frank Abanto <franabanto12@gmail.com>
2022-05-16 13:20:20 -0500

    first commit

---
+++ src/pages/view.js
@@ -0,0 +1,45 @@
+import { useParams } from "react-router-dom";
+import Layout from "../components/layout";
+import { useAppContext } from "../store/store";
+import { useState, useEffect } from "react";
+
+export default function View() {
+  const [item, setItem] = useState(null);
+  const params = useParams();
+  const store = useAppContext();
+
+  useEffect(() => {
+    const book = store.getItem(params.bookId);
+    setItem(book);
+  }, []);
+
+  const itemStyles = {
+    container: {
+      display: "flex",
+      gap: "20px",
+      color: "white",
+      width: "800px",
+      margin: "0 auto",
+    },
+  };
+
+  if (!item) {
+    return <Layout>Item not found</Layout>;
+  }
+  return (
+    <Layout>
+      <div style={itemStyles.container}>
+        <div>
+          <div>{item?.cover ? <img src={item?.cover} width="400" /> : ""}</div>
+        </div>
+        <div>
+          <h2>{item?.title}</h2>
+          <div>{item?.author}</div>
+          <div>{item?.intro}</div>
+          <div>{item?.completed ? "Leido" : "Por terminar"}</div>
+          <div>{item?.review}</div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
---
+++ src/store/store.js
@@ -0,0 +1,50 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import App from "../App";
+
+const AppContext = createContext({
+  items: [],
+  createItem: (item) => {},
+  getItem: (id) => {},
+  updateItem: (item) => {},
+});
+
+export default function Store({ children }) {
+  const [items, setItems] = useState([]);
+
+  function createItem(item) {
+    const temp = [...items];
+    temp.push(item);
+
+    setItems(temp);
+  }
+
+  function getItem(id) {
+    const item = items.find((item) => item.id === id);
+
+    return item;
+  }
+
+  function updateItem(item) {
+    const index = items.findIndex((i) => i.id === item.id);
+    const temp = [...items];
+
+    temp[index] = { ...item };
+  }
+
+  return (
+    <AppContext.Provider
+      value={{
+        items,
+        createItem,
+        getItem,
+        updateItem,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
